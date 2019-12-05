c8f10de7f82dc0779a2db7556e569596657b20d3
Qhub <qdaman90@gmail.com>
2019-12-04 23:57:01 -0600

    created the outline of the full game, the snake now moves across the screen, added a CSS page, cleaned code a bit(each function now does less)

--- game.js
+++ game.js
@@ -1,24 +1,67 @@
 let board;
 let boardContext;
 
+let snake = 50;
+let snakeSpeed = 5;
 
 window.onload = function() {
   board = this.document.getElementById('gameBoard');
-  boardContext = board.getContext('2d');
-   drawEverything()
+  boardContext = board.getContext('2d'); 
+
+  let framesPerSecond = 30;
+  this.setInterval(function() {
+    drawEverything();
+    moveEverything();
+  }, 1000/framesPerSecond);
+   
 }
 
 function drawEverything() {
-   boardContext.fillStyle = 'black';
-   boardContext.fillRect(0,0,board.width, board.height);
-   boardContext.fillStyle = 'red';
-   boardContext.fillRect(350,250,100,100);
-   boardContext.fillStyle = 'white';
-   boardContext.fillRect(250,350,100,100);
-   boardContext.fillStyle = 'white';
-   boardContext.fillRect(450,350,100,100);
-   boardContext.fillStyle = 'white';
-   boardContext.fillRect(250,150,100,100);
-   boardContext.fillStyle = 'white';
-   boardContext.fillRect(450,150,100,100);
+  // The Game Board
+  createPiece(0, 0, board.width, board.height, 'black');
+   
+  // The Prey/Food
+  createCircle(350,250,10,'red'); 
+
+
+// The Snake
+  createPiece(snake,200,50,25,'green');
+
+};
+
+function createCircle(centerX, CenterY, radius, color) {
+  boardContext.fillStyle = color;
+  boardContext.beginPath();
+  boardContext.arc(centerX,CenterY,radius,0,Math.PI*2, true);
+  boardContext.fill();
 }
+
+function moveEverything() {
+  snake += snakeSpeed;
+
+}
+
+function createPiece(leftX, topY, width, height, color) {
+  boardContext.fillStyle = color;
+  boardContext.fillRect(leftX, topY, width, height);
+}
+
+
+/*  Side Note - Need a function that will increase the snakes size when item is eaten.
+
+  function grow() {
+    if(snake eats )
+    snakeSize += ??
+    boardContext.fillRect(75,200,snakeSize,25);
+    
+  }
+
+  2nd** Condition for boundaries
+
+  {  
+  if(snake >== board.width) {
+    console.log('Games Over')
+  }
+
+  }
+*/
