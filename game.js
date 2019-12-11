3d5e5afe0ac9d98f2241773f415b41621f51736b
Qhub <qdaman90@gmail.com>
2019-12-10 21:32:31 -0600

    Snake now responds to movement but no longer moves across screen without input

--- game.js
+++ game.js
@@ -1,67 +1,122 @@
 let board;
 let boardContext;
 
-let snake = 50;
-let snakeSpeed = 5;
+const centerX = 400;
+const centerY = 300
+
+let snake = [];
+let snakeX = 250;
+let snakeY = 250;
+
 
 window.onload = function() {
   board = this.document.getElementById('gameBoard');
   boardContext = board.getContext('2d'); 
+  drawBoard();
+  createSnake();
+  createPrey();
 
-  let framesPerSecond = 30;
   this.setInterval(function() {
-    drawEverything();
-    moveEverything();
-  }, 1000/framesPerSecond);
+    drawBoard();
+    createSnake();
+    createPrey();
+
+  }, 1000);
+
+   document.addEventListener('keydown', (event) => {
+    snakeMovement(event)
+  })
    
 }
 
-function drawEverything() {
-  // The Game Board
-  createPiece(0, 0, board.width, board.height, 'black');
-   
-  // The Prey/Food
-  createCircle(350,250,10,'red'); 
+function drawBoard() {
+  boardContext.fillStyle = 'black';
+  boardContext.fillRect(0, 0, board.width, board.height);
+}
 
 
-// The Snake
-  createPiece(snake,200,50,25,'green');
+function createSnake(direction) {
+  snake = [ {x:snakeX, y: snakeY, snakeW: 50, snakeL:25 } ]
 
-};
+    snake.forEach(link => {
+    boardContext.fillStyle = 'green';
+    boardContext.fillRect(link.x, link.y, link.snakeW, link.snakeL);
+  })
 
-function createCircle(centerX, CenterY, radius, color) {
-  boardContext.fillStyle = color;
+  switch(direction){
+  case 'up':
+    snakeY -= 10;
+    break;
+  case 'down':
+    snakeY += 10;
+    break;
+  case 'left':
+    snakeX -= 10;
+    break
+  case 'right':
+    snakeX += 10;
+  default:
+    console.log('what?')
+} 
+  
+  console.log(snake)
+
+}
+
+function createPrey() {
+  boardContext.fillStyle = 'red';
   boardContext.beginPath();
-  boardContext.arc(centerX,CenterY,radius,0,Math.PI*2, true);
+  boardContext.arc(centerX,centerY,10,0,Math.PI*2, true);
   boardContext.fill();
 }
 
-function moveEverything() {
-  snake += snakeSpeed;
+// MOVEMENT
 
+// function checkBoundaries() {
+
+// // X Axis
+//   if(snakeX > board.width - 45 || snakeX < 0) {
+//     // alert('GAME OVER!!')
+//   }
+
+//   // Y Axis
+//   if(snakeY > board.height - 45 || snakeY < 0) {
+//     // alert('GAME OVER!!')
+//   }
+// }
+
+
+function snakeMovement(event) {
+  action = event.keyCode;
+  let direction;
+
+  switch(action) {
+    case 38:
+    case 87:
+      console.log('You pressed Up!');
+      direction = 'up';
+      createSnake(direction);
+      break;
+    case 40:
+    case 83:
+      console.log('You pressed Down!');
+      direction = 'down';
+      createSnake(direction);
+      break;
+    case 37:
+    case 65:
+      console.log('You pressed Left!');
+      direction = 'left';
+      createSnake(direction);
+      break;
+    case 39:
+    case 68:
+      console.log('You pressed Right!');
+      direction = 'right';
+      createSnake(direction);
+      break;
+    default:
+      console.log('default') ;
+  }
 }
 
-function createPiece(leftX, topY, width, height, color) {
-  boardContext.fillStyle = color;
-  boardContext.fillRect(leftX, topY, width, height);
-}
-
-
-/*  Side Note - Need a function that will increase the snakes size when item is eaten.
-
-  function grow() {
-    if(snake eats )
-    snakeSize += ??
-    boardContext.fillRect(75,200,snakeSize,25);
-    
-  }
-
-  2nd** Condition for boundaries
-
-  {  
-  if(snake >== board.width) {
-    console.log('Games Over')
-  }
-
-  }
-*/
